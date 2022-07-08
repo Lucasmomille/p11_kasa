6a59253acd61c2d66246167920e99f312a7f34cf
Lucas <lucas.moquet95@gmail.com>
2022-07-08 14:28:44 +0200

    save sass in dev dependencies

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,9 +4,9 @@ import './banner.scss'
 export default function Banner() {
 
     return (
-        <div className='banner'>
-            <div className='banner__container__img'>
-                <img src="/images/banner.png" alt="vue sur une montagne nuageuse" className='banner__image' />
+        <div className="banner">
+            <div className="banner__container__img">
+                <img src="/images/banner.png" alt="vue sur une montagne nuageuse" className="banner__image" />
             </div>
             <h2 className="banner__title">Chez vous, partout et ailleurs</h2>
         </div>
