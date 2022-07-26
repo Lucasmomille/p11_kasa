507f6b5ca65e8bf648ab282972b57365864abf64
Lucas <lucas.moquet95@gmail.com>
2022-07-26 12:29:25 +0200

    reorganize css

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import './banner.scss'
 
-export default function Banner({imgSrc, children}) {
+export default function Banner({imgSrc, home, children}) {
 
     return (
-        <div className="banner">
+        <div className={`banner ${home}`}>
             <img src={imgSrc} alt="" className="banner__image" />
 
             {children}
--- src/pages/home.js
+++ src/pages/home.js
@@ -6,16 +6,28 @@ import Card from '../components/card/Card'
 
 export default function Home() {
     const [data, setData] = useState([])
+    const [width, setWidth] = useState(window.innerWidth);
+
+    function handleWindowSizeChange() {
+        setWidth(window.innerWidth);
+    }
+    const isMobile = width <= 768;
+
     useEffect(() => {
         axios.get('/data/logements.json')
         .then(response => setData(response.data))
         .catch(e => console.log(e))
+
+        window.addEventListener('resize', handleWindowSizeChange);
+        return () => {
+            window.removeEventListener('resize', handleWindowSizeChange);
+        }
     }, [])
 
     return (
         <Layout>
-            <Banner imgSrc="/images/banner.png">
-                <h2 className="banner__title">Chez vous, partout et ailleurs</h2>
+            <Banner imgSrc="/images/banner.png" home="banner__home">
+                <h2 className="banner__title">Chez vous,{isMobile && <br></br>} partout et ailleurs</h2>
             </Banner>
             <div className="wrapper">
                 {data.map((card, dataIdx) => (
